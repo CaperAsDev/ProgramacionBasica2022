42d9201e19a84cf23cf1b9e5d4d2f482ad2835d6
Caperactus <102335770+Caperactus@users.noreply.github.com>
2022-07-25 16:04:12 -0500

    Juego terminado

--- jugando.js
+++ jugando.js
@@ -3,6 +3,7 @@
 let jugador = 0;
 let computador = 0;
 let escogida = ["🪨","🧻","✂️"];
+let ids = ["icono_jugador", "icono_pc"];
 let ganadas = 0;
 let perdidas = 0;
 let rondas = 3;
@@ -22,6 +23,38 @@ function contador (id, contando){
      document.getElementById(id).textContent= `${contando}`;   
 }
 
+//Hacemos una funcion para leer la seleccion del jugador
+
+function leerSeleccion(){
+    //Esto me trae un nodeList con los imputs que en su atributo name son iguales a "seleccion" de nuestro html, este nodelist parece un array pero no lo es asi que debo convertirlo a array.
+
+    let nodeList = document.querySelectorAll('input.seleccion');
+    let arrayOpciones = Array.apply(null, nodeList);
+    // console.log(arrayOpciones);
+
+//Traer el index de la opcion del array que tenga su propiedad checked en true
+
+    const seleccionado = arrayOpciones.findIndex((opcion) => opcion.checked === true);
+
+//Ahora pasamos el numero del index(seleccionado) a nuestra variable jugador
+
+    return jugador = seleccionado;
+
+}
+//Hacer funcion para que el resultado del duelo salga en pantalla
+function resultadoDuelo(resultado){
+    document.getElementById("resultadoDuelo").textContent=`${resultado}`
+}
+//Hacer la funcion para que aparezcan los iconos en pantalla
+function iconoEnPantalla(id, icon){
+    document.getElementById(id).textContent=`${icon}`
+}
+function icono(){
+    iconoEnPantalla(ids[0], escogida[leerSeleccion()]);
+    iconoEnPantalla(ids[1], "?");
+
+    
+}
 //Ahora haremos la funcion que activara el boton jugar de nuestra pagina
 
 function evaluar(){
@@ -29,33 +62,24 @@ function evaluar(){
         computador = aleatorio(0,2);
         console.log(escogida[computador]);
 
-    //Esto me trae un nodeList con los imputs que en su atributo name son iguales a "seleccion" de nuestro html, este nodelist parece un array pero no lo es asi que debo convertirlo a array.
-
-        let nodeList = document.querySelectorAll('input.seleccion');
-        let arrayOpciones = Array.apply(null, nodeList);
-        // console.log(arrayOpciones);
-
-    //Traer el index de la opcion del array que tenga su propiedad checked en true
-
-        const seleccionado = arrayOpciones.findIndex((opcion) => opcion.checked === true);
-
-    //Ahora pasamos el numero del index(seleccionado) a nuestra variable jugador
-
-        jugador = seleccionado;
-
+    //Leemos la seleccion del jugador
+    leerSeleccion()
+    //Pintamos icono de seleccion del computador en pantalla
+    iconoEnPantalla(ids[1], escogida[computador]);
     //Evaluando jugada
 
         if(jugador == computador){
-            alert(`🤬 Empate: Ambos escogieron ${escogida[jugador]} `)
+            resultadoDuelo("🤬 Empate")
+            
         }else if(jugador == 0 && computador == 2 || jugador == 1 && computador == 0 || jugador == 2 && computador == 1){
             ganadas ++
             contador("contador_ganadas-numero", ganadas);
-        alert(`🥳 Le ganaste con tu poderosa ${escogida[jugador]} a la debil ${escogida[computador]} del computador` );
+            resultadoDuelo("🥳 Ganaste");
         }
         else{
             perdidas ++
             contador("contador_perdidas-numero", perdidas);
-            alert(`😈 Destrozaron tu triste ${escogida[jugador]} con la vieja confiable ${escogida[computador]} del computador`)
+            resultadoDuelo("😈 Perdiste")
         }
    
         console.log(`ganadas: ${ganadas} perdidas: ${perdidas} vs ${rondas}`);
@@ -63,13 +87,11 @@ function evaluar(){
 
         if(ganadas == rondas || perdidas == rondas){
             if(ganadas > perdidas){
-                alert("¡Ganaste!")
-                ganadas = 0;
-                perdidas = 0;
+                alert("=====¡Ganaste!=====")
+                reiniciar();
             }else{
-                alert("¡Perdiste!")
-                ganadas = 0;
-                perdidas = 0;
+                alert("=====¡Perdiste!=====")
+                reiniciar();
             }
         }
         
@@ -79,9 +101,12 @@ function evaluar(){
 function reiniciar(){
     ganadas = 0;
     perdidas = 0;
+    iconoEnPantalla(ids[1], "?");
     contador("contador_ganadas-numero", ganadas);
     contador("contador_perdidas-numero", perdidas);
     rondas = prompt("¿Cuantas rondas quieres jugar?");
 
 }
+
+
 
